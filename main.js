ad8a2d21353ace48d904e5500787608a50541244
FrostSource <jjhfrost@gmail.com>
2022-05-19 17:35:40 +1200

    Add comments

--- main.js
+++ main.js
@@ -3,12 +3,14 @@
     console.log("Ready")
     document.querySelector("#EmailButtonSend").addEventListener("click", () => {
         let success = true;
+        // Clear success alert ahead of time.
         let alertSent = document.querySelector("#EmailAlertSent");
         alertSent.style.display = "none";
 
         let nameEl = document.querySelector("#EmailNameInput");
         let alertName = document.querySelector("#EmailAlertName");
         let name = nameEl.value.trim();
+        // Check if name is not blank and alert accordingly.
         if (name == "") {
             alertName.style.display = "block";
             success = false;
@@ -19,6 +21,7 @@
         let bodyEl = document.querySelector("#EmailBodyInput");
         let alertBody = document.querySelector("#EmailAlertBody");
         let body = bodyEl.value.trim();
+        // Check if body is not blank and alert accordingly.
         if (body == "") {
             alertBody.style.display = "block";
             success = false;
@@ -27,6 +30,7 @@
         }
 
         if (success) {
+            // Tailor success message to user's name.
             alertSent.innerHTML = `Thank you ${name}!`;
             alertSent.style.display = "block";
         }
